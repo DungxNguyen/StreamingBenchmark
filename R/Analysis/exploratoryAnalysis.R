c90f78ace5a41d2d7e7c1fcc528bb20225b1eb03
Dung Nguyen <dungx.ngt@gmail.com>
2017-06-20 23:45:28 -0400

    Create hourly count

--- R/Analysis/exploratoryAnalysis.R
+++ R/Analysis/exploratoryAnalysis.R
@@ -1,14 +1,18 @@
+library(ggplot2)
 Sys.setenv(SPARK_HOME = "/home/dnguyen/spark-2.1.1-bin-hadoop2.7")
 library(SparkR, lib.loc = c(file.path(Sys.getenv("SPARK_HOME"), "R", "lib")))
 sparkR.session(master = "local[*]", sparkConfig = list(spark.driver.memory = "2g"))
 
-# Create R data frame
-data <- data.frame(id = 1:100,
-                   Date = base::sample(seq(as.Date("2017-01-01"), as.Date("2017-01-31"), by = "day"), 100, replace = TRUE),
-                   level = ifelse(runif(10)<0.3, "WARN", "ERROR"))
+NUMBER_OF_SAMPLES <- 100
 
-# Order by Date
-data <- data[with(data, order(Date)),]
+timestamp <- sort(base::sample(seq(as.POSIXct("2017-01-01 00:00:00"), as.POSIXct("2017-01-31 23:59:59"), by = 1), NUMBER_OF_SAMPLES, replace = TRUE))
+head(timestamp)
+
+# Create R data frame
+data <- data.frame(id = 1:NUMBER_OF_SAMPLES,
+                   timestamp = timestamp,
+                   Date = as.Date(timestamp),
+                   level = ifelse(runif(NUMBER_OF_SAMPLES)<0.3, "WARN", "ERROR"))# Create R data frame
 
 # Convert R dataframe to Spark DataFrame
 sparkData <- as.DataFrame(data)
@@ -55,3 +59,44 @@ head(collect(newData))
 # SQL
 createOrReplaceTempView(sparkData, "sparkDataTable")
 collect(sql("SELECT * FROM sparkDataTable WHERE level == 'ERROR'"))
+
+# count data by day
+countByDay <- collect(count(group_by(sparkData, "Date")))
+head(countByDay)
+ggplot(data = countByDay, aes(x=Date, y=count)) + geom_line() + geom_point()
+
+# count data by day and level
+countByDayAndLevel <- collect(count(group_by(sparkData, "Date", "level")))
+head(countByDayAndLevel)
+ggplot(data = countByDayAndLevel, aes(x = Date, y = count, color = level)) + geom_point() + geom_line()
+
+# count data by hour and level 
+# create hour column
+# care about type of data.frame
+# it's easy to convert list to data.frame
+hourConvert <- function(x){
+  hour <- as.POSIXct(x$timestamp, origin = "1970-01-01")
+  # type <- class(x$timestamp)
+  # result <- as.data.frame(hour, stringsAsFactors = FALSE)
+  # result <- data.frame( list(Hour = c("abc"), Count = as.integer(c(1))), stringsAsFactors = FALSE)
+  # return(data.frame(list(as.character(type), as.integer(1)), stringsAsFactors = FALSE))
+  hour <- format(hour, "%Y-%m-%D %H")
+  hour <- as.data.frame(table(hour), stringsAsFactors = FALSE)
+  return(hour)
+}
+hourSchema <- structType(structField("Hour", "string"), structField("Count", "integer"))
+hourData <- dapply(sparkData, hourConvert, hourSchema)
+collect(hourData)
+
+# Function that create error
+# Spark's Timestamp ---> R's numeric
+# It may be a bug
+# The real "can not serialize factor" error doesn't come with prettyNum exception
+x <- data.frame(list(timestamp = c(1:100)))
+hourConvertError <- function(x){
+  as.data.frame(table(format(x$timestamp, "%Y-%m-%D %H")), stringsAsFactors = FALSE)  
+}
+hourConvertError(x)
+hourData <- dapply(sparkData, hourConvertError, hourSchema)
+collect(hourData)
+# End of error code segment
